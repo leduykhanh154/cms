84b46417dbd2d4f37ea63423ee4789fd290316af
leduykhanh154 <khanhle@mpire.vn>
2025-03-13 14:14:31 +0700

    update-conftest-pagev2

--- tests/test_pagev2.py
+++ tests/test_pagev2.py
@@ -1,15 +1,12 @@
-import time
 import pytest
 import logging
 from utils.login import Login
 from pages.pagev2 import PageV2
 from utils.driver_setup import get_driver
-from selenium.webdriver.common.by import By
 from locators.locator_pagev2 import LocatorPageV2
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
-
+ 
 @pytest.fixture(scope="function")
 def setup_driver():
     driver = get_driver()
@@ -48,7 +45,7 @@ def test_save_page_and_check_in_list(pagev2, setup_driver):
     pagev2.enter_page_title(valid_title)
     pagev2.click_save_button()
     expected_url = "https://mpire-cms-demo.mpire.asia/cms/admin-page-v2?page=1"
-    WebDriverWait(setup_driver, 5).until(EC.url_to_be(expected_url))
+    WebDriverWait(setup_driver, 10).until(EC.url_to_be(expected_url))
     if pagev2.is_page_title_in_list(valid_title):
         logging.info("Test Case 2 PASS: Lưu trang thành công và hiển thị trong danh sách.")
     else:
@@ -75,7 +72,10 @@ def test_add_section_popup_displayed(setup_driver, pagev2):
 # Test Case 4: Verify sau khi thêm section News -> Hệ thống hiển thị section News bên ngoài Danh sách section
 def test_add_news_section_and_verify_display(setup_driver, pagev2):
     pagev2.perform_tag_operations()
-    pagev2.add_news_section()
+    pagev2.click_add_section_button()
+    pagev2.is_add_section_popup_displayed()
+    pagev2.click_section_news_checkbox()
+    pagev2.click_add_button()
     news_section_element = WebDriverWait(setup_driver, 10).until(
         EC.visibility_of_element_located(LocatorPageV2.NEWS_SECTION)
     )
@@ -83,23 +83,28 @@ def test_add_news_section_and_verify_display(setup_driver, pagev2):
     if news_section_element and news_section_element.is_displayed():
         logging.info("Test Case 4 PASS: Section 'News' đã được thêm thành công và hiển thị trong danh sách.")
     else:
-        logging.error("Test Case 4 FAILED: Section 'News' không hiển thị trong danh sách.")
+        logging.error("Test Case 4 PASS: Section 'News' không hiển thị trong danh sách.")
         assert False, "Section 'News' không hiển thị sau khi thêm!"
 
 # Test Case 5: Verify khi không nhập Tiêu đề tin tức -> Hệ thống hiển thị thông báo lỗi Vui lòng nhập tiêu đề tin tức
 def test_news_section_title_validation(setup_driver, pagev2):
-    pagev2.perform_tag_operations()
-    pagev2.add_news_section()
+    pagev2.click_content_menu()
+    pagev2.click_page_v2_menu()
+    assert pagev2.click_create_new_button()
+    assert pagev2.click_add_section_button()
+    assert pagev2.is_add_section_popup_displayed()
+    assert pagev2.click_section_news_checkbox()
+    assert pagev2.click_add_button()
     news_section_element = pagev2.is_news_section_displayed()
     assert news_section_element is not None
     assert pagev2.click_save_button()
     error_message = pagev2.get_news_section_error_message(news_section_element)
     if error_message == "Vui lòng nhập tiêu đề tin tức":
-        logging.info("Test Case 5 PASS: Hiển thị đúng thông báo lỗi: Vui lòng nhập tiêu đề tin tức.")
+        logging.info("PASS: Hiển thị đúng thông báo lỗi: Vui lòng nhập tiêu đề tin tức.")
     else:
-        logging.error(f"Test Case 5 FAILED: Không hiển thị hoặc hiển thị sai thông báo lỗi: {error_message}")
+        logging.error(f"FAILED: Không hiển thị hoặc hiển thị sai thông báo lỗi: {error_message}")
         assert False, "Không tìm thấy hoặc thông báo lỗi không đúng."
-    
+
 # Test Case 6: Verify khi click icon rename -> Hệ thống hiển thị pop-up Chỉnh sửa tên section
 def test_rename_section_popup_display(setup_driver, pagev2):
     pagev2.perform_tag_operations()
@@ -108,9 +113,9 @@ def test_rename_section_popup_display(setup_driver, pagev2):
     assert news_section_element is not None
     pagev2.click_rename_section()
     popup_displayed = pagev2.is_rename_popup_displayed()
+    
     if popup_displayed:
         logging.info("Test Case 6 PASS: Pop-up Rename hiển thị thành công.")
     else:
         logging.error("Test Case 6 FAILED: Pop-up Rename không hiển thị.")
         assert False, "Pop-up Rename không hiển thị."
-    
